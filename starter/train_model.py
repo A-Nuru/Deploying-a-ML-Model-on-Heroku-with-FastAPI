473c7ba9e708ebb12f6e120ad4a41bb340ccb483
A-Nuru <access2abey@gmail.com>
2022-11-23 13:53:14 +0000

    changing the data path for the get train test data function

--- starter/train_model.py
+++ starter/train_model.py
@@ -16,10 +16,31 @@ def get_train_test_data(root_path):
     -------
     train_df , test_df
     """
-    data = pd.read_csv(f"{root_path}/data/clean/census.csv")
+    data = pd.read_csv(f"{root_path}/data/clean_census.csv")
     train_df, test_df = train_test_split(data, test_size=0.20)
 
     return train_df, test_df
+
+def train_save_model(train, cat_features, root_path):
+    """
+    Process data, train and save ml model
+    Parameters
+    ----------
+    root_path
+    train
+    cat_features
+    Returns
+    -------
+    """
+    x_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+    # train model
+    trained_model = train_model(x_train, y_train)
+    # save model
+    dump(trained_model, f"{root_path}/model/model.joblib")
+    dump(encoder, f"{root_path}/model/encoder.joblib")
+    dump(lb, f"{root_path}/model/lb.joblib")
 
 cat_features = [
     "workclass",
