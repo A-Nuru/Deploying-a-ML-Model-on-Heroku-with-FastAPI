015ccb7163fe07dd1861be4c0fe72d49a58ec27c
A-Nuru <access2abey@gmail.com>
2022-12-14 19:58:19 +0000

    correct  script url

--- valdate_heroku.py
+++ valdate_heroku.py
@@ -4,20 +4,19 @@ Heroku Api test script
 import requests
 
 data = {
-            'age': 52,
-            'workclass': 'Self-emp-not-inc',
-            'fnlgt': 209642,
-            'education': 'HS-grad',
-            'marital_status': 'Married-civ-spouse',
-            'occupation': 'Exec-managerial',
-            'relationship': 'Husband',
-            'race': 'White',
-            'sex': 'Male',
-            'hoursPerWeek': 45,
-            'nativeCountry': 'United-States'
-    }
-
-r = requests.post('https://salaryclassapp.herokuapp.com/', json=data)
+"age": 52,
+"workclass": "Self-emp-not-inc",
+"fnlgt": 209642,
+"education": "HS-grad",
+"marital_status": "Married-civ-spouse",
+"occupation": "Exec-managerial",
+"relationship": "Husband",
+"race": "White",
+"sex": "Male",
+"hoursPerWeek": 45,
+"nativeCountry": "United-States"
+}
+r = requests.post('https://salaryclassapp.herokuapp.com/inference', json=data)
 
 assert r.status_code == 200
 
