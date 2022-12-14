a6693423da36b57d137b4f38388e36d9a1f986f5
A-Nuru <access2abey@gmail.com>
2022-12-14 18:51:04 +0000

    adding script to validate Post request locally

---
+++ validate_post.py
@@ -0,0 +1,25 @@
+"""
+Local Api test script
+"""
+import requests
+
+data = {
+            'age': 52,
+            'workclass': 'Self-emp-not-inc',
+            'fnlgt': 209642,
+            'education': 'HS-grad',
+            'marital_status': 'Married-civ-spouse',
+            'occupation': 'Exec-managerial',
+            'relationship': 'Husband',
+            'race': 'White',
+            'sex': 'Male',
+            'hoursPerWeek': 45,
+            'nativeCountry': 'United-States'
+    }
+    
+r = requests.post('http://127.0.0.1:8000/inference', json=data)
+
+assert r.status_code == 200
+
+print("Response code: %s" % r.status_code)
+print("Response body: %s" % r.json())
