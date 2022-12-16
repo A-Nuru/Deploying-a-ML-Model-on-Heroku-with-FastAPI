dad42936cd93134fc96f7084f97e571d989f3e6b
A-Nuru <access2abey@gmail.com>
2022-12-16 20:13:19 +0000

    writing batch/single predictions to a file

--- starter/inference_model.py
+++ starter/inference_model.py
@@ -1,7 +1,11 @@
+"""
+Contains a function to make batch or single prediction and
+write it into a file 
+"""
 from joblib import load
 from .ml.data import process_data
 from .ml.model import inference
-
+import logging
 
 def run_inference(data, cat_features, root_dir):
     """
@@ -13,24 +17,21 @@ def run_inference(data, cat_features, root_dir):
     cat_features
     Returns
     -------
-    prediction
-    """
-    """model = load("model/model.joblib")
-    encoder = load("model/encoder.joblib")
-    lb = load("model/lb.joblib")"""
-    
+    """ 
     trained_model = load(f"{root_dir}/model/model.joblib")
     encoder = load(f"{root_dir}/model/encoder.joblib")
     lb = load(f"{root_dir}/model/lb.joblib")
 
-    X,_, _, _ = process_data(
-        data,
-        categorical_features=cat_features,
-        encoder=encoder, lb=lb, training=False)
-
-    pred = inference(trained_model, X)
-    prediction = lb.inverse_transform(pred)[0]
-    print(pred)
-    print(prediction)
-
-    return prediction
+    with open(f'{root_dir}/model/predictions_output.txt', 'w') as file:
+        X_test,y_test, _, _ = process_data(
+	data,
+	categorical_features=cat_features, label="salary",
+	training=False, encoder=encoder, lb=lb) 
+	
+        pred = inference(trained_model, X_test)
+        prediction = lb.inverse_transform(pred)[0]
+        predictions = lb.inverse_transform(pred)
+        predictions = "The test set predictions are %s" % (predictions)
+        logging.info(predictions)
+        file.write(predictions + '\n') 
+        
