828c273c8b45453217732cd638e747385917bf00
A-Nuru <access2abey@gmail.com>
2022-12-16 20:16:11 +0000

    including root_dir as parameter in the val_model function

--- starter/validate_model.py
+++ starter/validate_model.py
@@ -1,6 +1,6 @@
 import logging
 from joblib import load
-from .ml.model import compute_score_per_slice
+from .ml.model import compute_score_per_slice, write_model_metrics
 from starter.ml.model import inference
 from starter.train_model import get_train_test_data
 from starter.ml.data import process_data
@@ -20,25 +20,12 @@ def val_model(test_df, cat_features, root_dir):
     trained_model = load(f"{root_dir}/model/model.joblib")
     encoder = load(f"{root_dir}/model/encoder.joblib")
     lb = load(f"{root_dir}/model/lb.joblib")
+     
+    logging.info("Overall score check procedure started")
+    write_model_metrics(trained_model, test_df, encoder,
+                            lb, cat_features, root_dir)
     
-    #_, test_data = get_train_test_data(root_path)
-    
-    
-    
-    """x_test, y_test, encoder, lb = process_data(
-        train, categorical_features=cat_features, label="salary", training=False
-    )
-    
-    
-    
-    preds = inference(trained_model, x_test)"""
-
-    #preds = run_inference(test_data, cat_features)
-    
-    #logging.info("Overall score check procedure started")
-    #compute_model_metrics(y_test, preds)
-    
-    #logging.info("Slice score check procedure started")
+    logging.info("Slice score check procedure started")
     compute_score_per_slice(
         trained_model,
         test_df,
@@ -46,10 +33,4 @@ def val_model(test_df, cat_features, root_dir):
         lb,
         cat_features,
         root_dir)
-           
-def val_overall_model(root_path, cat_features):
-     _, test_data = get_train_test_data(root_path)
-     y_test, preds = run_inference(test_data, cat_features, root_path)
-     return y, preds
-     
-     
+       
